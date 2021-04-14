39d19da67bdae3f901ecf5f652908624f65068c1
Dabeen Jeong <hello70825@gmail.com>
2021-04-14 16:33:46 +0900

    update

--- 15900/15905.py
+++ 15900/15905.py
@@ -1,12 +1,11 @@
-T=int(input())
-A=[[0]*2 for i in range(T)];B=[];C=[]
-for i in range(T):
-    x,y=map(int,input().split())
-    A[i][0]=x;C.append(x)
-    A[i][1]=y
-A.sort(reverse=True)
-for i in range(5):
-    B.append(A[0][0])
-    C.remove(A[0][0])
-    del A[0]
-print(C.count(B[4]))
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+arr = sorted(arr, key=lambda x:[-x[0],x[1]])
+
+ans = 0
+for i in range(5,len(arr)):
+    if arr[4][0] == arr[i][0]:
+        ans += 1
+print(ans)
--- 5500/5598.py
+++ 5500/5598.py
@@ -1,14 +1,4 @@
-a = str(input())
-T = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-for i in range(len(a)):
-    if a[i] == "A":
-        print("X", end="")
-    elif a[i] == "B":
-        print("Y", end="")
-    elif a[i] == "C":
-        print("Z", end="")
-    else:
-        for j in range(26):
-            if a[i] == T[j]:
-                print(chr(65+j-3),end="")
-print("")
+arr = list(input())
+for i in range(len(arr)):
+    arr[i] = chr(((ord(arr[i]) - 42) % 26) + 65)
+print(''.join(arr))
