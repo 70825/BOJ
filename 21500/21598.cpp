97636c94133d73b8d6a6349badadcf02a66180a4
Dabeen Jeong <hello70825@gmail.com>
2021-07-22 17:30:52 +0900

    update

---
+++ 21500/21598.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int n;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	cout << "SciComLove" << endl;
+	if (n == 2) cout << "SciComLove" << endl;
+}
