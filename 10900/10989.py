516537a9bc9759e66414b78612f738bec10fe2ca
Dabeen Jeong <hello70825@gmail.com>
2021-09-03 13:48:59 +0900

    counting_sort

--- 10900/10989.py
+++ 10900/10989.py
@@ -1,6 +1,9 @@
-input=__import__('sys').stdin.readline
-D=[0]*10001
-for _ in range(int(input())):
-    D[int(input())]+=1
-for i in range(1,10001):
-    print('{}\n'.format(i)*D[i],end='')
+input = __import__('sys').stdin.readline
+n = int(input())
+arr = [0] * 10001
+for i in range(n):
+    x = int(input())
+    arr[x] += 1
+for i in range(10001):
+    for j in range(arr[i]):
+        print(i)
