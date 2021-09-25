60bfb2097bbfcf08521665c9729868d918cb010a
Dabeen Jeong <hello70825@gmail.com>
2021-09-25 19:55:28 +0900

    implementation

--- 10200/10214.py
+++ 10200/10214.py
@@ -1,18 +1,8 @@
-T = int(input())
-Yonsei = 0
-Korea = 0
-for j in range(T):
-    for i in range(9):
-        A = input()
-        x, y = A.split(' ')
-        if 0<= int(x) <=9 and 0<=int(y)<=9:
-            if int(x) > int(y):
-                Yonsei += 1
-            elif int(x) < int(y):
-                Korea += 1
-    if Yonsei == Korea:
-        print("Draw")
-    elif Yonsei > Korea:
-        print("Yonsei")
-    elif Korea > Yonsei:
-        print("Korea")
+for _ in range(int(input())):
+    score = [[*map(int, input().split())] for _ in range(9)]
+    y = sum(score[i][0] for i in range(9))
+    k = sum(score[i][1] for i in range(9))
+
+    if y > k: print('Yonsei')
+    elif k > y: print('Korea')
+    else: print('Draw')
