c3eeaa0b0823d851718875d0087c15ba4aff355d
Dabeen Jeong <hello70825@gmail.com>
2021-10-03 15:39:12 +0900

    backtracking

--- 23000/23028.py
+++ 23000/23028.py
@@ -1,13 +1,12 @@
-def go(seme_idx, major_gpa, total_gpa):
-    if seme_idx + n == 8:
-        if major_gpa >= 66 and total_gpa >= 130:
-            print('Nice')
-            exit()
+def go(x, seme, major, total):
+    if seme == 8:
+        if major >= 66 and total >= 130: print('Nice'); exit()
         return
-    for i in range(semester[seme_idx][0] + 1):
-        go(seme_idx + 1, major_gpa + 3 * i, total_gpa + 3 * (min(6 - i, semester[seme_idx][1]) + i))
+    for i in range(arr[x][0] + 1):
+        non_major = min(6 - i, arr[x][1])
+        go(x+1, seme+1, major + 3*i, total + (i + non_major) * 3)
 
 n, a, b = map(int, input().split())
-semester = [[*map(int, input().split())] for _ in range(10)]
-go(0, a, b)
+arr = [[*map(int, input().split())] for _ in range(10)]
+go(0, n, a, b)
 print('Nae ga wae')
