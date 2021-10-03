1104a8d3be2457c1ed94e0560e863b45d4effd38
Dabeen Jeong <hello70825@gmail.com>
2021-10-03 15:48:56 +0900

    recursion

--- 23000/23028.py
+++ 23000/23028.py
@@ -1,12 +1,12 @@
-def go(x, seme, major, total):
-    if seme == 8:
+def go(x, major, total):
+    if x + n == 8:
         if major >= 66 and total >= 130: print('Nice'); exit()
         return
     for i in range(arr[x][0] + 1):
         non_major = min(6 - i, arr[x][1])
-        go(x+1, seme+1, major + 3*i, total + (i + non_major) * 3)
+        go(x+1, major + 3*i, total + (i + non_major) * 3)
 
 n, a, b = map(int, input().split())
 arr = [[*map(int, input().split())] for _ in range(10)]
-go(0, n, a, b)
+go(0, a, b)
 print('Nae ga wae')
