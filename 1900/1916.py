c6f51a3c09d8a53c52c1fd1237ea68557552f094
Dabeen Jeong <hello70825@gmail.com>
2021-11-15 18:03:23 +0900

    dijkstra

--- 1900/1916.py
+++ 1900/1916.py
@@ -7,9 +7,10 @@ for i in range(m):
     a,b,c=map(int,input().split())
     D[a].append((b,c))
 s,e=map(int,input().split())
-q=[];heappush(q,[0,s])
+q=[];heappush(q,[0,s]); S[s] = 0
 while q:
     t,x=heappop(q)
+    if S[x] != t: continue
     for nx,nt in D[x]:
         if S[nx]>nt+t:S[nx]=nt+t;heappush(q,[nt+t,nx])
 print(S[e])
